4e34b9da610063204b883e4495122d7bfa679b73
dat05 <80480354+dat05@users.noreply.github.com>
2021-07-20 19:26:04 +0700

    Update l4l.py

--- l4l.py
+++ l4l.py
@@ -10,7 +10,7 @@ reset='\u001b[0m'
 def loading(): 
   print (green+"Loading...") 
   for i in range(0, 100): 
-    time.sleep(0.1) 
+    time.sleep(0.02) 
     sys.stdout.write(u"\u001b[1000D" + str(i + 1) + "%") 
     sys.stdout.flush()
     print
